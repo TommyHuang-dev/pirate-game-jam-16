fb0dcb4ad2a42c17305f32375643b75e1929e37c
Tommy Huang <tommyhuang999@gmail.com>
2025-01-19 01:40:03 -0500

    add dashing by holding space

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,15 +7,18 @@ public class Character : MonoBehaviour {
     private Collider2D _collider;
     private Rigidbody2D _rb;
     private Vector2 _respawnPoint;
+    private Camera camera;
 
     [Header("Movement Variables")]
-    public float maxMoveSpeed = 5.0f;
-    public float acceleration = 0.9f; // value between 0 and 1 used by lerp
-    public float accelerationMaxSpeedInc = 2.0f; // when moving the player, a higher acceleration value is used
-    public float deceleration = 4.0f; // speed at which the player can actively stop and change direction. Currently not doing anything
-    public float friction = 0.05f; // how fast the player slows down naturally if they stop accelerating
+    public float maxMoveSpeed = 4.0f;
+    public float acceleration = 1.0f; // value between 0 and 1 used by lerp (technically it could be higher than 1)
+    public float accelerationMaxSpeedInc = 6.0f; // when moving the player, a higher acceleration value is used
+    public float deceleration = 4.0f; // speed at which the player can actively stop and change direction. Currently does nothing.
+    public float friction = 0.8f; // how fast the player slows down naturally if they stop accelerating
 
-    //public Vector2 currentVelocity = new Vector2(0.0f, 0.0f); // should probably use rb velocity instead?
+    private static float globalGravity = -9.81f;
+    private float gravityScale = 0.0f;  // default to no gravity since we are "suspended" in fluid
+    //public Vector2 veldebug;
 
     private enum PlayerState
     {
@@ -27,14 +30,29 @@ public class Character : MonoBehaviour {
     }
     [SerializeField] private PlayerState currentState = PlayerState.Stopped;
 
-    private static float globalGravity = -9.81f;
-    private float gravityScale = 0.0f;  // default to no gravity since we are "suspended" in fluid
+    private enum DashState
+    {
+        None,
+        Charging,
+        Dashing,
+        Recovery
+    }
+    [SerializeField] private DashState currentDashState = DashState.None;
 
-    public Vector2 veldebug;
-
+    [Header("Dash Variables")]
     #region Dash Attack
-
-
+    // Variables during charge
+    public Vector2 dashCharge = new Vector2(0.0f, 0.8f);
+    public float dashChargePerSec = 1.0f;
+    public float dashChargeSlow = 0.6f; // speed penalty while charging
+    // Variables during dash
+    public float dashMinDistance = 2.0f;
+    public float dashMaxDistance = 8.0f;
+    public Vector2 dashDuration = new Vector2(0.0f, 0.6f); // remaining / max duration
+    // Variables during recovery and cooldown
+    public Vector2 dashCooldown = new Vector2(0.0f, 0.4f); // remaining / max cooldown
+    public Vector2 dashDrift = new Vector2(0.0f, 0.4f); // removes speed cap to allow for gradual slow down
+    private Vector2 dashVector = new Vector2(0, 0);
     #endregion
 
     // Called before first frame update
@@ -42,53 +60,26 @@ public class Character : MonoBehaviour {
         _rb = this.GetComponent<Rigidbody2D>();
         _animator = GetComponentInChildren<Animator>();
         _collider = GetComponent<Collider2D>();
-
-        // Set respawn point to initial position
-        //SetRespawnPoint(transform.position);
+        if (camera == null)
+        {
+            camera = Camera.main; // Automatically assign the main camera if not set
+        }
 
         // Turn off regular gravity so we can use a custom implementation
         _rb.gravityScale = 0;
     }
     // Called once per frame
     private void Update() {
-        //if (currentState == State.NoControl) {
-        //    NoControl = 0;
-        //    return;
-        //}
-
-        //// Jump check
-        //if (Input.GetKeyDown(KeyCode.Space)) {
-        //    Jump();
-        //}
-
-        //// Fell off map check
-        //if (_rb.position.y < -20) {
-        //    Die();
-        //}
-
-        //// Clamp fall speed
-        //if (_rb.linearVelocity.y < _maxFallSpeed) {
-        //    _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _maxFallSpeed);
-        //}
+        calcDashState();
     }
     private void FixedUpdate() {
-        // update the current player state if needed
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) < 0.0001f && Mathf.Abs(Input.GetAxis("Vertical")) < 0.0001f) {
-            if (_rb.linearVelocity.magnitude < 0.0001f) {
-                currentState = PlayerState.Stopped;
-            } else {
-                currentState = PlayerState.Drifting;
-            }
-        } else if (currentState != PlayerState.NoControl)
-        {
-            currentState = PlayerState.Moving;
-        }
+        calcPlayerState();
 
         // get values from keeb
         var targetVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         targetVelocity.Normalize();
         targetVelocity *= (maxMoveSpeed + accelerationMaxSpeedInc);
-        var targetVelocityLerp = Vector2.Lerp(_rb.linearVelocity, targetVelocity, acceleration);
+        var targetVelocityLerp = Vector2.LerpUnclamped(_rb.linearVelocity, targetVelocity, acceleration);
         var velocityDiff = targetVelocityLerp - _rb.linearVelocity;
 
         //targetHSpeed = Mathf.Lerp(_rb.linearVelocity.x, targetHSpeed, 1);
@@ -97,63 +88,139 @@ public class Character : MonoBehaviour {
 
         #region Friction
         // if the player isn't actively accelerating, add friction
-        if (currentState != PlayerState.Moving) {
-            float frictionAmount = Mathf.Min(Mathf.Abs(_rb.linearVelocity.magnitude), Mathf.Abs(friction));
+        if (currentState == PlayerState.Stopped)
+        {
+            _rb.linearVelocity.Set(0, 0);
+        }
+        else if (currentState != PlayerState.Moving && currentDashState != DashState.Dashing) {
+            float frictionAmount = Mathf.Max(_rb.linearVelocity.magnitude * friction, friction);
+            if (frictionAmount * Time.fixedDeltaTime > _rb.linearVelocity.magnitude) {
+                _rb.linearVelocity.Set(0, 0);
+            }
             var direction = _rb.linearVelocity;
             direction.Normalize();
-            _rb.AddForce(-frictionAmount * direction, ForceMode2D.Impulse);
+            
+            _rb.linearVelocity += -frictionAmount * direction * Time.fixedDeltaTime;
         }
         #endregion
-
-        var movement = velocityDiff * acceleration;
-        _rb.AddForce(movement, ForceMode2D.Force);
+        #region movement
+        if (currentState != PlayerState.NoControl && currentDashState != DashState.Dashing) {
+            var movement = velocityDiff * acceleration;
+            if (currentDashState == DashState.Charging) {
+                movement *= (1 - dashChargeSlow);
+            }
+            //_rb.AddForce(movement, ForceMode2D.Force);
+            _rb.linearVelocity += movement * Time.fixedDeltaTime;
+        }
 
         // clamp velocity to max
-        _rb.linearVelocity = Vector2.ClampMagnitude(_rb.linearVelocity, maxMoveSpeed);
+        if (currentDashState != DashState.Dashing && currentDashState != DashState.Recovery) {
+            var tempMaxSpeed = maxMoveSpeed;
+            if (currentDashState == DashState.Charging)
+            {
+                tempMaxSpeed *= (1 - dashChargeSlow);
+            }
+            _rb.linearVelocity = Vector2.ClampMagnitude(_rb.linearVelocity, tempMaxSpeed);
+        } 
+        else if (currentDashState == DashState.Recovery) {
+            var tempMaxSpeed = maxMoveSpeed + Mathf.Max(0, (dashVector.magnitude - maxMoveSpeed) * (dashDrift.x / dashDrift.y));
+            _rb.linearVelocity = Vector2.ClampMagnitude(_rb.linearVelocity, tempMaxSpeed);
+        }
 
-        //// DEBUG
-        //print(targetVelocity + " t");
-        //print(_rb.linearVelocity + " v");
-        //print(hMovement + " m");
-        //print(hSpeedDiff + " d");
-        //print(hAccelRate + " a");
-        //// For animator on movement update
-        //_animator.SetFloat("speed", _rb.linearVelocity.magnitude);
-        //veldebug = _rb.linearVelocity;
+        // dash movement
+        if (currentDashState == DashState.Dashing)
+        {
+            dashDuration.x -= Time.fixedDeltaTime;
+            _rb.linearVelocity = dashVector;
+            if (dashDuration.x <= 0)
+            {
+                dashCooldown.x = dashCooldown.y;
+
+                dashDrift.x = dashDrift.y;
+                currentDashState = DashState.Recovery;
+            }
+            // TODO damage on enemy contact
+        }
+        #endregion
     }
 
-    //#region Movement
-    //private void Jump()
-    //{
-    //    //_rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-    //}
-    //#endregion
+    private void calcDashState()
+    {
+        #region Dash Calcs
+        if (currentDashState == DashState.None || currentDashState == DashState.Recovery)
+        {
+            dashCooldown.x -= Time.deltaTime;
+            if (Input.GetKey(KeyCode.Space) && dashCooldown.x <= 0)
+            {
+                dashCharge.x = 0.0f;
+                currentDashState = DashState.Charging;
+            }
+        }
+        if (currentDashState == DashState.Charging)
+        {
+            // change state to dash and set dash vector if button released
+            dashCharge.x = Mathf.Min(dashCharge.y, dashCharge.x + Time.deltaTime);
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                var dashDistance = dashMinDistance + (dashMaxDistance - dashMinDistance) * (dashCharge.x / dashCharge.y);
+                var dashSpeed = dashDistance / dashDuration.y;
 
-    //#region Dying!
-    //private void SetRespawnPoint(Vector2 point)
-    //{
-    //    _respawnPoint = point;
-    //}
+                Vector3 mouseScreenPos = Input.mousePosition;
+                Vector3 mouseWorldPos = camera.ScreenToWorldPoint(mouseScreenPos);
+                var dashDirection = mouseWorldPos - transform.position;
+                dashDirection.z = 0;
+                dashDirection.Normalize();
+                dashVector = dashDirection * dashSpeed;
 
-    //private void MiniJump()
-    //{
-    //    _rb.AddForce(6f * Vector2.up, ForceMode2D.Impulse);
-    //}
-    //public void Die()
-    //{
-    //    _canMove = false;
-    //    _collider.enabled = false;
-    //    MiniJump();
-    //    StartCoroutine(Respawn());
-    //}
+                dashDuration.x = dashDuration.y;
+                currentDashState = DashState.Dashing;
 
-    //private IEnumerator Respawn()
-    //{
-    //    yield return new WaitForSeconds(1f);
-    //    _rb.linearVelocity = new Vector2(0f, 0f);
-    //    transform.position = _respawnPoint;
-    //    _canMove = true;
-    //    _collider.enabled = true;
-    //}
-    //#endregion
+                // DEBUG STUFF
+                Debug.DrawLine(transform.position, transform.position + (dashDuration.y * dashSpeed * dashDirection), Color.cyan, dashDuration.y);
+                // ***
+            }
+            // DEBUG STUFF ***
+            else
+            {
+                var dashDistance = dashMinDistance + (dashMaxDistance - dashMinDistance) * (dashCharge.x / dashCharge.y);
+                var dashSpeed = dashDistance / dashDuration.y;
+                Vector3 mouseScreenPos = Input.mousePosition;
+                Vector3 mouseWorldPos = camera.ScreenToWorldPoint(mouseScreenPos);
+                var dashDirection = mouseWorldPos - transform.position;
+                dashDirection.z = 0;
+                dashDirection.Normalize();
+                Debug.DrawLine(transform.position, transform.position + (dashDuration.y * dashSpeed * dashDirection), Color.white);
+            }
+            // ***
+        }
+        if (currentDashState == DashState.Recovery)
+        {
+            dashDrift.x -= Time.deltaTime;
+            if (dashDrift.x <= 0)
+            {
+                currentDashState = DashState.None;
+            }
+        }
+        #endregion
+    }
+
+    private void calcPlayerState()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) < 0.01f && Mathf.Abs(Input.GetAxis("Vertical")) < 0.01f)
+        {
+            if (_rb.linearVelocity.magnitude < 0.01f)
+            {
+                currentState = PlayerState.Stopped;
+            }
+            else
+            {
+                currentState = PlayerState.Drifting;
+            }
+        }
+        else if (currentState != PlayerState.NoControl)
+        {
+            currentState = PlayerState.Moving;
+        }
+    }
+
 }
